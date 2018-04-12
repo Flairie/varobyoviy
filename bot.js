e580842886979ba49a54cb5431122b81c541cb9e
Flairie <37420327+Flairie@users.noreply.github.com>
2018-04-12 19:11:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const version = '0.1.0';
 var Team = {};
 var Role = {};
 var Luvr = {};
+var Inve = {};
 let hellowed = false;
 
 client.on('ready', () => {
@@ -11,7 +13,7 @@ client.on('ready', () => {
 
 client.on('message', msg => { 
   if(hellowed == false) {
-    msg.channel.send("Я обновился, а Ваши экспириенсы были сброшены :/");
+    msg.channel.send("Я обновился (Версия " + version + "), а Ваши экспириенсы были сброшены :/");
     hellowed = true;
   }
   
@@ -19,6 +21,7 @@ client.on('message', msg => {
     Team[msg.author.id] = 0;
     Role[msg.author.id] = 0;
     Luvr[msg.author.id] = 0;
+    Inve[msg.author.id] = [0, 0, 0, 0, 0, 0, 0, 0];
     msg.reply('Привет, друг! Либо ты новенький, либо твои данные были сброшены! Приятного общения!');
   }
 });
