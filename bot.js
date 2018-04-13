07cd02036666cfab3df00f620f73a0b64038f9f9
Flairie <37420327+Flairie@users.noreply.github.com>
2018-04-13 18:44:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
-const version = '0.2.0';
+const version = '0.2.1: job system';
 const ItemName = ["Пусто", "Диск"];
 const Rank = ["434273045159346181", "434273241121554444", "422839749536120832", "422832838971228171", "422850622216339467"];
 var Team = {};
