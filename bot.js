9e5de2c1ebe3466f029ee412e2aaec3eada13877
Flairie <37420327+Flairie@users.noreply.github.com>
2018-04-13 18:27:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const version = '0.1.1';
+const version = '0.2.0';
 const ItemName = ["Пусто", "Диск"];
 const Rank = ["434273045159346181", "434273241121554444", "422839749536120832", "422832838971228171", "422850622216339467"];
 var Team = {};
@@ -9,6 +9,8 @@ var Luvr = {};
 var Ruby = {};
 var Inve = {};
 var Invn = {};
+var WorkP = [];
+var PokrC = 0;
 let hellowed = false;
 
 function InvenoryString(id, USER) {
@@ -30,6 +32,8 @@ client.on('message', msg => {
     msg.channel.send("Я обновился (Версия " + version + "), а Ваши экспириенсы были сброшены :/");
     hellowed = true;
   }
+  
+  PokrC -= 1;
   
   if(!(msg.author.id in Team || msg.author.bot)) {
     Team[msg.author.id] = 0;
@@ -54,14 +58,26 @@ client.on('message', msg => {
   } else {
     if(msg.content == ":inv") {
       InvenoryString(msg.author.id, msg.author);
+    } else if(msg.content == ":job") {
+      msg.reply("Список работ:\n1. Реклама Покровского (1rub, 5luv). Пишите :workP");
+    } else if(msg.content == ":workP") {
+      msg.author.sendMessage('Вы наняты. За каждое сообщение "Голосуйте за Покровского!" вы будете получать указаную зарплату, но при условии, что предыдущая реклама была достаточно давно');
+      WorkP += msg.author.id;
     } else if(msg.content == ":invpubl") {
       InvenoryString(msg.author.id, msg.channel);
-    } if(msg.content == ":mny") {
+    } else if(msg.content == ":mny") {
       msg.author.sendMessage("Рубины: " + Ruby[msg.author.id].toString() + "\nЛувры: "+ Luvr[msg.author.id].toString());
     } else if(msg.content == ":mnypubl") {
       msg.channel.sendMessage("Рубины: " + Ruby[msg.author.id].toString() + "\nЛувры: "+ Luvr[msg.author.id].toString());
     } else if(msg.content == ":help") {
-      msg.reply("Список команд:\n:help - Список команд\n:inv - Приватный инвентарь\n:mny - Приватные деньги\n:invpubl - Публичный инвентарь\n:mnypubl - Публичные деньги")
+      msg.reply("Список команд:\n:help - Список команд\n:job - Список работ\n:inv - Приватный инвентарь\n:mny - Приватные деньги\n:invpubl - Публичный инвентарь\n:mnypubl - Публичные деньги")
+    }
+    
+    if(msg.content === "Голосуйте за Покровского!" && msg.author.id in WorkP && PokrC <= 0) {
+      Luvr[msg.author.id] += 5;
+      Ruby[msg.author.id] += 1
+      msg.author.sendMessage("Средства начислены!)";
+      PokrC = 20;
     }
   }
 });
