dbeb91a9a710a342d7aa7e65f841f81f3e9c5f07
Flairie <37420327+Flairie@users.noreply.github.com>
2018-04-13 18:55:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,11 +73,11 @@ client.on('message', msg => {
       msg.reply("Список команд:\n:help - Список команд\n:job - Список работ\n:inv - Приватный инвентарь\n:mny - Приватные деньги\n:invpubl - Публичный инвентарь\n:mnypubl - Публичные деньги");
     }
     
-    if(msg.content === "Голосуйте за Покровского!" && msg.author.id in WorkP && PokrC <= 0) {
-      //Luvr[msg.author.id] += 5;
-      //Ruby[msg.author.id] += 1;
+    if(msg.content === "Голосуйте за Покровского!" && PokrC <= 0) {
+      Luvr[msg.author.id] += 5;
+      Ruby[msg.author.id] += 1;
       msg.author.sendMessage("Средства not начислены!");
-      //PokrC = 20;
+      PokrC = 20;
     }
   }
 });
