b7ed24f3b38a3f9e635e372822e4dab1355cff52
Flairie <37420327+Flairie@users.noreply.github.com>
2019-01-19 15:41:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
 
+function clean_empty(array) {
+  for (i = 0; i < array.length; i++) {
+    if(array[i] == "") {
+      array.splice(i,1);
+      i--;
+    }
+  }
+}
 
 client.on('ready', () => {
   //console.log(`Logged in as ${423868710940311552}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content == "Вахтанг лох") {
-       msg.delete(4);
-    }
+  if (msg.content.search("Вахтанг") != -1 && (msg.content.search("лох") != -1 || msg.content.search("дубина") != -1)) {
+     msg.delete(4);
+  }
 });
 
 client.login(process.env.BOT_TOKEN); // wow
